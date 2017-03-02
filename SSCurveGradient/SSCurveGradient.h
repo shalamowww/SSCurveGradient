9cf6d9176cd93de47731265884431b41604138cc
Alexander <xxx@xxx.local>
2017-03-02 03:23:26 +0300

    initial

---
+++ SSCurveGradient/SSCurveGradient.h
@@ -0,0 +1,19 @@
+//
+//  SSCurveGradient.h
+//  SSCurveGradient
+//
+//  Created by Alexander on 3/2/17.
+//  Copyright © 2017 curvegradient. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSCurveGradient.
+FOUNDATION_EXPORT double SSCurveGradientVersionNumber;
+
+//! Project version string for SSCurveGradient.
+FOUNDATION_EXPORT const unsigned char SSCurveGradientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSCurveGradient/PublicHeader.h>
+
+
